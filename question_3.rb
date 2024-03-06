0b652405cd633d6f1cffc9475772808874e87602
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-03-05 23:32:56 -0300

    feat: question 3

---
+++ question_3.rb
@@ -0,0 +1,29 @@
+# 3) Descubra a lógica e complete o próximo elemento:
+
+# a) 1, 3, 5, 7, ___
+# r) O proximo valor é 11, de acordo com a sequencia dos numeros primos
+
+
+# b) 2, 4, 8, 16, 32, 64, ____
+# r) o proximo valor vai ser 128, sequencia do dobro do numero anterior.
+
+
+# c) 0, 1, 4, 9, 16, 25, 36, ____
+
+# 13
+# 11
+# 9
+# 7
+# 5
+# 3
+
+# r) proximo numero vai ser 49, soma dos quadrados perfeitos
+
+# d) 4, 16, 36, 64, ____
+# r) proximo numero vai ser 100, numeros multiplos de 2 ao quadrado somando mais dois, 2 ao quadrado, 4 ao quadrado
+
+# e) 1, 1, 2, 3, 5, 8, ____
+# r) proximo numero vai ser 13, sequencia de Fibonacci
+
+# f) 2, 10, 12, 16, 17, 18, 19, ____
+# r) Proximo numero vai ser 200, numeros que tem som que começa com a letra 'd'.
