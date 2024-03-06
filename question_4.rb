a6198eeea3d82b32a8968939d20ca3b610284231
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-03-05 23:33:05 -0300

    feat: question 4

---
+++ question_4.rb
@@ -0,0 +1,18 @@
+# 4) Você está em uma sala com três interruptores,
+# cada um conectado a uma lâmpada em uma sala diferente.
+# Você não pode ver as lâmpadas da sala em que está, mas
+# pode ligar e desligar os interruptores quantas vezes quiser.
+# Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+
+# Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+
+
+# Levando em consideração que lampadas geralmente esquentam poderia ser
+# feito o seguinte.
+
+# - ligo o primeiro interruptor e espero um pouco, tempo sufiente para esquentar
+# - ligo a segundo interruptor e vou a sala
+
+# se a lampada estiver ligada o interruptor 2 é o responsavel por ela
+# se ela estiver apagada mas quente o interruptor 1 é responasvel por ela
+# caso contrario, ela estiver apagada e fria o interruptor 3 é responsavel por ela.
